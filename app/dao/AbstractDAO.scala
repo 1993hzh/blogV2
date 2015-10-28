35c4205c861ac18ba08377eb41daca2be28cefe7
Leo <mail@huzhonghua.cn>
2015-10-28 18:25:37 +0800

    add CRUD method in abstract class for subclass to implement

--- app/dao/AbstractDAO.scala
+++ app/dao/AbstractDAO.scala
@@ -1,14 +1,28 @@
 package dao
 
+import models.AbstractModel
 import play.api.Play
 import play.api.db.slick.{HasDatabaseConfig, DatabaseConfigProvider}
 import _root_.slick.driver.JdbcProfile
 
+import scala.concurrent.Future
+
 /**
  * Created by leo on 15-10-27.
  */
-trait AbstractDAO extends HasDatabaseConfig[JdbcProfile] {
+
+trait AbstractDAO[M <: AbstractModel] extends HasDatabaseConfig[JdbcProfile] {
 
   override protected val dbConfig = DatabaseConfigProvider.get[JdbcProfile]("blog")(Play.current)
+
+  def insert(model: M): Future[Unit]
+
+  def update(model: M): Future[Int]
+
+  def query(hql: String): Future[Seq[M]]
+
+  def delete(model: M): Future[Int]
+
+  def upsert(model: M): Future[Int]
 
 }
