a67309088a0c6feae153128e94042279c193e685
Leo <mail@huzhonghua.cn>
2015-11-03 20:16:02 +0800

    remove long name dir; start working on front-end

---
+++ app/controllers/Index.scala
@@ -0,0 +1,14 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+  * Created by leo on 15-11-3.
+  */
+object Index extends Controller {
+
+  def index = Action {
+    Ok(views.html.index())
+  }
+
+}
