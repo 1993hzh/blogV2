896588a5160b7f5f374d7c7e541397c779bd0edc
Leo <mail@huzhonghua.cn>
2015-11-05 16:26:49 +0800

    update Global

--- app/Global.scala
+++ app/Global.scala
@@ -1,10 +1,8 @@
 import play.api._
 import play.api.mvc._
 
-import play.api.Logger
-import scala.concurrent.Future
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
-object Global extends WithFilters() {
+object Global extends WithFilters() with GlobalSettings {
+
 
 }
