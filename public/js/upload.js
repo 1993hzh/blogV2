c647e5553ab75483a7d5be1a4f453c1819456e4d
Leo <mail@huzhonghua.cn>
2015-12-02 00:03:16 +0800

    update progressbar & popup timeout

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,6 +1,7 @@
 function showUploadProgress(file) {
     var html = "<div id=\""+ file.id +"\"class=\"progress\"><div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" "
-        + "aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\"><span class=\"sr\">"+ file.name +"</span></div></div>";
+        + "aria-valuenow=\"0\" aria-valuemin=\"0\" aria-valuemax=\"100\"><span class=\"sr\">"+ file.name +"&nbsp;-&nbsp;</span>" +
+        "<label class=\"percent\">0%</label></div></div>";
     $("#fileUploadProgress").append(html);
 }
 
@@ -12,7 +13,7 @@ var updateProgress = function(id, percent) {
     }
     progressbar.attr("aria-valuenow", percent);
     progressbar.width(percent + "%");
-    parent.find("span .sr").html(parent + "%");
+    parent.find("label[class='percent']").html(percent + "%");
 }
 
 var addImage = function(src) {
